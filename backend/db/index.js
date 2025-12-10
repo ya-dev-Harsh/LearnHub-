3b8ccb7f52b0aeebb10a2a8ab7243723f4499458
ya-dev-Harsh <yadav.hk05@gmail.com>
2025-12-10 23:28:41 +0530

    update

--- backend/db/index.js
+++ backend/db/index.js
@@ -2,8 +2,11 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false, },
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 module.exports = {
